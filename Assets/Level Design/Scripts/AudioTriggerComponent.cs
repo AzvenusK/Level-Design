51b86b10dc6dd761622158f4ee6730c6e59035d8
azvenuskestel <kanmol36@gmail.com>
2019-07-25 01:21:34 +0530

    Commit 4
    Complete Level Design in Assets.

---
+++ Assets/Level Design/Scripts/AudioTriggerComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTriggerComponent : MonoBehaviour
+{
+    public AudioClip newAudio = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            GetComponent<AudioSource>().PlayOneShot(newAudio);
+        }
+    }
+
+ 
+
+
+}
---
+++ Assets/Level Design/Scripts/ThunderClap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour 
+{
+	bool canFlicker = true;
+	private Light myLight;
+	public AudioClip audioClip;
+	void Update()
+	{
+		StartCoroutine (Flicker ());
+	}
+
+	IEnumerator Flicker()
+	{
+		if (canFlicker) {
+			canFlicker = false;
+			GetComponent<AudioSource>().PlayOneShot (audioClip);
+			myLight = GetComponent<Light> ();
+			myLight.enabled = true;
+			yield return new WaitForSeconds (Random.Range (0.1f, 0.4f));
+			myLight.enabled = false;
+			yield return new WaitForSeconds (Random.Range (1, 5));
+			canFlicker = true;
+		}
+	}
+}
